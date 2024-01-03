a20b4b0195464d8e15096c2391a73ccf1911720a
Mehrbod <118477122+ProfessorMystic@users.noreply.github.com>
2024-01-03 01:39:06 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,28 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up button click listeners
-        setupButtonClickListeners();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Set up button click listeners
-        setupButtonClickListeners();
-    }
-
-    private void setupButtonClickListeners() {
         Button startButton = findViewById(R.id.startbutton);
         Button quitButton = findViewById(R.id.quitbutton);
+
+        // Display a Toast message
+        Toast.makeText(MainActivity.this, "Not Based On Anything Serious", Toast.LENGTH_SHORT).show();
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Navigate to next page
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivityForResult(intent, 1); // 1 is the requestCode
+                startActivity(intent);
             }
         });
 
